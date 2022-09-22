e82b5e9d5f10f09ea7bd3be2b3a07591cdac5ba2
Oleg Martynov <fjolne.yngling@gmail.com>
2022-09-22 13:24:05 +0100

    fixed a type in ThirdPersonController

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -129,7 +129,7 @@ namespace StarterAssets {
 
             _hasAnimator = TryGetComponent(out _animator);
             _input = GetComponent<StarterAssetsInputs>();
-#if ENABLE_INPUYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
             _playerInput = GetComponent<PlayerInput>();
 #else
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
