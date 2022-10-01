2e42ff87f2aba8ccd037911f9e52b913623f5952
Oleg Martynov <fjolne.yngling@gmail.com>
2022-10-01 22:02:23 +0100

    src cleanup

--- Assets/_Main/Scripts/Network/Character/NetworkCharacterLocomotionHandler.cs
+++ Assets/_Main/Scripts/Network/Character/NetworkCharacterLocomotionHandler.cs
@@ -4,56 +4,31 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace _Main.Scripts.Network.Character {
-    /// <summary>
-    /// Enables the UltimateCharacterLocomotionHandler to be aware of the character on the network.
-    /// </summary>
     public class NetworkCharacterLocomotionHandler : MonoBehaviour {
-        private INetworkInfo m_NetworkInfo;
-        [System.NonSerialized] private PlayerInput m_PlayerInput;
-        private UltimateCharacterLocomotion m_CharacterLocomotion;
-        private ThirdPersonController m_ThirdPersonController;
+        private INetworkInfo networkInfo;
+        private PlayerInput playerInput;
+        private UltimateCharacterLocomotion characterLocomotion;
+        private ThirdPersonController thirdPersonController;
 
-        /// <summary>
-        /// Initializes the default values.
-        /// </summary>
         protected void Awake() {
-            m_CharacterLocomotion = GetComponent<UltimateCharacterLocomotion>();
-            m_ThirdPersonController = GetComponent<ThirdPersonController>();
-            m_NetworkInfo = GetComponent<INetworkInfo>();
-            m_PlayerInput = GetComponent<PlayerInput>();
+            characterLocomotion = GetComponent<UltimateCharacterLocomotion>();
+            thirdPersonController = GetComponent<ThirdPersonController>();
+            networkInfo = GetComponent<INetworkInfo>();
+            playerInput = GetComponent<PlayerInput>();
 
             EventHandler.RegisterEvent(gameObject, "OnCharacterSpawned", OnCharacterSpawned);
-
-            // enabled = false;
-            // m_CharacterLocomotion.enabled = false;
         }
 
         private void OnCharacterSpawned() {
-            if (m_NetworkInfo.IsLocalPlayer()) {
-                var _playerCamera = FindObjectOfType<CinemachineVirtualCamera>();
-                _playerCamera.Follow = m_CharacterLocomotion.cameraRoot;
+            if (networkInfo.IsLocalPlayer()) {
+                var playerCamera = FindObjectOfType<CinemachineVirtualCamera>();
+                playerCamera.Follow = characterLocomotion.cameraRoot;
             }
             else {
-                // Non-local players will not be controlled with player input.
                 enabled = false;
-                m_PlayerInput.enabled = false;
-                m_CharacterLocomotion.enabled = false;
-                m_ThirdPersonController.enabled = false;
-            }
-        }
-
-        /// <summary>
-        /// Determines if the handler should be enabled.
-        /// </summary>
-        private void Start() {
-        }
-
-        /// <summary>
-        /// The character has respawned.
-        /// </summary>
-        protected void OnRespawn() {
-            if (!m_NetworkInfo.IsLocalPlayer()) {
-                return;
+                playerInput.enabled = false;
+                characterLocomotion.enabled = false;
+                thirdPersonController.enabled = false;
             }
         }
     }
